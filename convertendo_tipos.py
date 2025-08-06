edb5fc5aba661195fd73ca3602f40ed75a621b72
VitorBando <vitorm.804@gmail.com>
2025-08-06 13:41:32 -0300

    introduzindo conversões de tipos de dados

---
+++ convertendo_tipos.py
@@ -0,0 +1,11 @@
+print(int(1.3216516)) # convertendo um decimal para inteiro, porem não é uma boa pratica devido a perca de dados precisos
+print(int('10')) # convertendo um string para inteiro
+print(float('10.156165')) # convertendo um string para decimal
+print(float(100)) # convertendo um inteiro para decimal
+
+valor = 10 
+valor_str = str(valor) # convertendo inteiro para string
+
+# conferido os tipos das variáveis
+print(type(valor))
+print(type(valor_str))
