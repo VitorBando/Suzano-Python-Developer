2b003bcfe87427dab55f33a9a35e700eba6b8b84
VitorBando <vitorm.804@gmail.com>
2025-08-08 13:25:00 -0300

    operadores de comparação

---
+++ Capitulo2/operadores_comparacao.py
@@ -0,0 +1,17 @@
+saldo = 200
+saque = 200
+
+# Igual
+print(saldo == saque)
+
+# Maior ou Igual
+print(saldo >= saque)
+
+# Menor ou Igual
+print(saldo <= saque)
+
+# Menor
+print(saldo < saque)
+
+# Maior
+print(saldo > saque)
