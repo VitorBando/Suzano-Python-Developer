3e5cfd76a9d52feb8fb526cb45f6e56d3774094b
VitorBando <vitorm.804@gmail.com>
2025-08-08 14:59:25 -0300

    operadores de associação

---
+++ Capitulo2/operadores_associacao.py
@@ -0,0 +1,12 @@
+frutas = ['limao', 'uva']
+print('laranja' in frutas)
+
+frutas = ['limao', 'uva', 'laranja']
+print('laranja' in frutas)
+
+frutas = ['limao', 'uva']
+print('laranja' not in frutas)
+
+curso = 'Curso Python'
+print('Python' in curso) 
+print('python' in curso) # case sensitive
