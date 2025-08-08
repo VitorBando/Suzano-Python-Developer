4d3acfc4191b9e9d50200ba708e87bd7ea50dbc0
VitorBando <vitorm.804@gmail.com>
2025-08-08 13:22:35 -0300

    operadores aritmeticos e procedencias

---
+++ Capitulo2/operadores_aritmeticos.py
@@ -0,0 +1,23 @@
+produto1 = 20
+produto2 = 10
+
+# adição
+print(produto1 + produto2)
+
+# subtração
+print(produto1 - produto2)
+
+# multiplicação
+print(produto1 * produto2)
+
+# divisão
+print(produto1 / produto2)
+
+# divisão inteira
+print(produto1 // produto2)
+
+# modulo
+print(produto1 % produto2)
+
+# exponenciação
+print(produto1 ** produto2)
