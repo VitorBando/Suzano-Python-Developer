5ebcfd8c59021227a5cf436cd0de936be5db23bf
VitorBando <vitorm.804@gmail.com>
2025-08-08 15:26:28 -0300

    indentação e blocos

---
+++ Capitulo2/identacao_blocos.py
@@ -0,0 +1,12 @@
+saldo = 500
+
+def sacar(valor):
+    # abertura do bloco if
+    if saldo >= valor:
+        # abertura do retorno da condição if
+        print('Valor sacado')
+    # fechamento bloco if
+    print('Obrigado por ser nosso cliente.')
+# fechamento da função sacar
+
+sacar(100)        
