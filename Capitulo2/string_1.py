84ad38ec860fa9339574eadb612773c1539d6756
VitorBando <vitorm.804@gmail.com>
2025-08-14 20:44:31 -0300

    Conhecendo métodos úteis da classe string

---
+++ Capitulo2/string_1.py
@@ -0,0 +1,26 @@
+nome = 'Vitor'
+
+print(nome.upper())
+print(nome.lower())
+print(nome.title())
+
+texto = ' Ola, mundo!     '
+
+print(texto)
+print(texto.strip() + '.')
+print(texto.rstrip() + '.')
+print(texto.lstrip() + '.')
+
+menu = 'Python'
+
+print('####' + menu + '####')
+print(menu.center(14))
+print(menu.center(14, '#'))
+
+print('P-y-t-h-o-n')
+
+for l in menu:
+    print(l, end='-')
+print()
+
+print('-'.join(menu))
