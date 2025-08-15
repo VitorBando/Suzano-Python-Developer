ea2eb76f76efbaa638f755d7ac0bdbf3dc567fb3
VitorBando <vitorm.804@gmail.com>
2025-08-15 19:22:05 -0300

    args - tuplas e kwargs - dict para parametros.

---
+++ Capitulo2/03_args_kwargs.py
@@ -0,0 +1,16 @@
+
+def exibir_poema(data_extenso, *args, **kwargs):
+    texto = '\n'.join(args)
+    meta_dados = '\n'.join([f'{chave.title()}: {valor}' for chave, valor in kwargs.items()])
+    menssagem = f'{data_extenso}\n\n{texto}\n\n{meta_dados}'
+    print(menssagem)
+
+exibir_poema(
+    'Sexta-feira, 15 de Agosto de 2025',
+    'Zen of Python',
+    'Beatiful is better than ugly.',
+
+    autor = 'Tim Peters',
+    ano=1999,
+
+)
