947ddc4701207da572f97a62a4cce3253922e475
VitorBando <vitorm.804@gmail.com>
2025-09-02 11:24:21 -0300

    aplicando properties em funções para apliar base de POO

---
+++ Capitulo4/03 - Encapsulamento/03_properties_ex_person.py
@@ -0,0 +1,16 @@
+class Pessoa:
+    def __init__(self, nome, ano_nascimento):
+        self._nome = nome
+        self._ano_nascimento = ano_nascimento
+
+    @property
+    def nome(self):
+        return self._nome
+    
+    @property
+    def idade(self):
+        _ano_atual = 2025
+        return _ano_atual - self._ano_nascimento
+    
+p = Pessoa('Vitor', 1999)
+print(f"Nome: {p.nome}\nIdade: {p.idade}")
